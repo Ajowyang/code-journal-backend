fcbf89ad9f7711ff3afe4fdb0de0fbb71fcc3101
ajowyang <andrewowyang875@gmail.com>
2024-09-20 17:19:21 +0000

    server.ts added CREATE and part of READ(readAll)

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import pg from 'pg';
 import express from 'express';
 import { ClientError, errorMiddleware } from './lib/index.js';
+import { networkInterfaces } from 'os';
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -15,8 +16,39 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/api/create/', async (req, res, next) => {});
-// app.get()
+app.post('/api/create/', async (req, res, next) => {
+  try {
+    const body = req.body;
+    if (!body) {
+      throw new Error('No body');
+    }
+    if (!body.title || !body.notes || !body.photoUrl) {
+      throw new Error('Title, notes, and photoUrl are required');
+    }
+    const sql = `
+  insert into "entries"
+  ("title", "notes", "photoUrl")
+   values ($1, $2, $3)
+   returning *`;
+    const result = await db.query(sql, [body.title, body.notes, body.photoUrl]);
+    res.status(201).json('entry added');
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/readAll', async (req, res, next) => {
+  try {
+    const sql = `
+    select *
+    from "entries"
+    `;
+    const result = await db.query(sql);
+    res.json(result.rows);
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.use(errorMiddleware);
 
